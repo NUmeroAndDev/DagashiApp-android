bb287adb9f87a20f7be21563e07524b00553aaa8
NUmeroAndDev <numero.dev.and@gmail.com>
2022-02-12 11:55:29 +0900

    Implemented splash screen

---
+++ app/src/main/java/jp/numero/dagashiapp/DagashiApplication.kt
@@ -0,0 +1,8 @@
+package jp.numero.dagashiapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DagashiApplication : Application() {
+}
--- app/src/main/java/jp/numero/dagashiapp/MainActivity.kt
+++ app/src/main/java/jp/numero/dagashiapp/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import dagger.hilt.android.AndroidEntryPoint
 import jp.numero.dagashiapp.ui.MilestoneListScreen
 import jp.numero.dagashiapp.ui.theme.DagashiAppTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        installSplashScreen()
         setContent {
             DagashiAppTheme {
                 // A surface container using the 'background' color from the theme
