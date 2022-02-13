314e7e0aae686380735bffed19ebc82c63ee09da
NUmeroAndDev <numero.dev.and@gmail.com>
2022-02-13 11:16:11 +0900

    Connected repository to ViewModel

--- ui/src/main/java/jp/numero/dagashiapp/ui/MilestoneListViewModel.kt
+++ ui/src/main/java/jp/numero/dagashiapp/ui/MilestoneListViewModel.kt
@@ -3,9 +3,8 @@ package jp.numero.dagashiapp.ui
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import jp.numero.dagashiapp.model.Milestone
 import jp.numero.dagashiapp.model.MilestoneList
-import kotlinx.coroutines.delay
+import jp.numero.dagashiapp.repository.DagashiRepository
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
@@ -13,6 +12,7 @@ import javax.inject.Inject
 
 @HiltViewModel
 class MilestoneListViewModel @Inject constructor(
+    private val dagashiRepository: DagashiRepository
 ) : ViewModel() {
 
     private val _uiState: MutableStateFlow<UiState<MilestoneList>> = MutableStateFlow(UiState())
@@ -33,20 +33,8 @@ class MilestoneListViewModel @Inject constructor(
     private fun load(isRefresh: Boolean = false) {
         _uiState.value = uiState.value.copy(isLoading = true, isRefreshing = isRefresh)
         viewModelScope.launch {
-            // TODO: impl load
             val result = runCatching {
-                delay(1000L)
-                MilestoneList(
-                    (0..21).map {
-                        Milestone(
-                            id = it.toString(),
-                            number = 100 + it,
-                            description = "Description",
-                            path = "",
-                            closedAd = "2020-09-13"
-                        )
-                    }
-                )
+                dagashiRepository.fetchMilestoneList()
             }
             _uiState.value = uiState.value.copy(
                 isLoading = false,
