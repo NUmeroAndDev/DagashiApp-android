c021bad28baf441a33ee4540819401399edd0502
NUmeroAndDev <numero.dev.and@gmail.com>
2022-04-21 20:58:42 +0900

    Implemented material3-window-size-class

--- ui/src/main/java/jp/numero/dagashiapp/ui/WindowSizeClass.kt
+++ ui/src/main/java/jp/numero/dagashiapp/ui/WindowSizeClass.kt
@@ -1,39 +1,13 @@
 package jp.numero.dagashiapp.ui
 
 import android.app.Activity
+import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
+import androidx.compose.material3.windowsizeclass.WindowSizeClass
+import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.remember
-import androidx.compose.ui.geometry.Size
-import androidx.compose.ui.graphics.toComposeRect
-import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.unit.DpSize
-import androidx.compose.ui.unit.dp
-import androidx.window.layout.WindowMetricsCalculator
 
-enum class WindowSizeClass { Compact, Medium, Expanded }
-
+@OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
 @Composable
 fun Activity.rememberWindowSizeClass(): WindowSizeClass {
-    val windowSize = rememberWindowSize()
-    val windowDpSize = with(LocalDensity.current) {
-        windowSize.toDpSize()
-    }
-    return getWindowSizeClass(windowDpSize)
-}
-
-@Composable
-private fun Activity.rememberWindowSize(): Size {
-    val configuration = LocalConfiguration.current
-    val windowMetrics = remember(configuration) {
-        WindowMetricsCalculator.getOrCreate().computeCurrentWindowMetrics(this)
-    }
-    return windowMetrics.bounds.toComposeRect().size
-}
-
-private fun getWindowSizeClass(windowDpSize: DpSize): WindowSizeClass = when {
-    windowDpSize.width < 0.dp -> throw IllegalArgumentException("Dp value cannot be negative")
-    windowDpSize.width < 600.dp -> WindowSizeClass.Compact
-    windowDpSize.width < 840.dp -> WindowSizeClass.Medium
-    else -> WindowSizeClass.Expanded
+    return calculateWindowSizeClass(this)
 }
