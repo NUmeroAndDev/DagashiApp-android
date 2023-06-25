6cddf5e2901f15f86785ed18c0cfffba3331d2ae
NUmeroAndDev <numero.dev.and@gmail.com>
2023-06-25 11:57:26 +0900

    Replaced model module with Composite Build

---
+++ model/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("jp.numero.dagashiapp.buildlogic.primitive.androidlibrary")
+    id("jp.numero.dagashiapp.buildlogic.primitive.kotlin")
+}
+
+android {
+    namespace = "jp.numero.dagashiapp.model"
+}
+
+dependencies {
+    implementation(libs.androidx.annotation)
+
+    testImplementation("junit:junit:4.13.2")
+}
