399e7de7a7595c20708d2c7b61958b9477e50bbf
NUmeroAndDev <numero.dev.and@gmail.com>
2024-05-21 09:37:26 +0900

    Migrated Outmirrored icon

--- ui/src/main/java/jp/numero/dagashiapp/ui/component/TopAppBar.kt
+++ ui/src/main/java/jp/numero/dagashiapp/ui/component/TopAppBar.kt
@@ -1,9 +1,19 @@
 package jp.numero.dagashiapp.ui.component
 
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.RowScope
+import androidx.compose.foundation.layout.WindowInsets
+import androidx.compose.foundation.layout.WindowInsetsSides
+import androidx.compose.foundation.layout.only
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material3.*
+import androidx.compose.material.icons.automirrored.outlined.ArrowBack
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.TopAppBarScrollBehavior
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -24,7 +34,7 @@ fun TopAppBar(
         {
             IconButton(onClick = onBack) {
                 Icon(
-                    imageVector = Icons.Outlined.ArrowBack,
+                    imageVector = Icons.AutoMirrored.Outlined.ArrowBack,
                     contentDescription = stringResource(id = R.string.navigate_up),
                 )
             }
